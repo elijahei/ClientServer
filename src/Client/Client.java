ced641e58461aa17ea32c18684469d851d722819
elijahei <einsteine98@gmail.com>
2023-11-25 21:13:00 -0500

    getters setters

--- src/Client/Client.java
+++ src/Client/Client.java
@@ -1,5 +1,33 @@
 package Client;
 
+import java.net.InetAddress;
+
 class Client{
+
+    private InetAddress destAdress;
+    private Integer portNumber;
+
+    Client(InetAddress destAddress, Integer portNumber){
+       this.destAdress = destAddress;
+        this.portNumber = portNumber;
+    }
+
+    public InetAddress getDestAdress() {
+        return destAdress;
+    }
+
+    public void setDestAdress(InetAddress destAdress) {
+        this.destAdress = destAdress;
+    }
+
+    public Integer getPortNumber() {
+        return portNumber;
+    }
+
+    public void setPortNumber(Integer portNumber) {
+        this.portNumber = portNumber;
+    }
+
+    
 
 }
